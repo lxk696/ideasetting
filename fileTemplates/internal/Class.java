ab9c31e4140517d771a7b23eeb660aedd46644ad
lxk-ecs <69671710@qq.com>
2019-02-05 23:28:36 +0800

    IU-2018.3.3 <admin@DESKTOP-MNNED62 Create project.default.xml, diff.xml, Enum.java, Default copy.xml, Interface.java, lxk-customer.xml, shkstart.xml, Default.xml, applicationLibraries.xml, file.template.settings.xml, find.xml, keymap.xml, javarebel.xml, abbreviations.xml, Default.xml, scala.xml, Eclipse copy.xml, who-did-it.xml, editor.codeinsight.xml, grepConsole.xml, editor.xml, uml.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, other.xml, git.xml, _@user_Default.icls, ide-features-trainer.xml, eclipseCodeFormatter.xml, lombok-plugin.xml, other.xml, ui.lnf.xml, filetypes.xml, projectView.xml, plugin_ui.xml, notifications.xml, scala_config.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, output.xml, laf.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @description  $DESCRIPTION
+ * @date      ${YEAR}年${MONTH}月${DAY}日
+ * @author   刘雄康
+ * @version  v1.0
+ */
+public class ${NAME} {
+}
