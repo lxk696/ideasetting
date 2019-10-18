5687072fce4cb0e9eaf0835d27e0bc460cd48e1b
lxk-421 <69671710@qq.com>
2019-10-18 21:23:43 +0800

    IU-2019.2.3 <Administrator@PC-20170812UQOA Create project.default.xml, Enum.java, Default copy.xml, Interface.java, lxk-customer.xml, shkstart.xml, Default.xml, find.xml, mavenVersion.xml, GrepConsole.xml, Default.xml, BBT_Style.xml, MyBatisCodeHelper.xml, lxk-customer-scala.xml, editor.codeinsight.xml, OGNL.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, alibaba-toolkit-tips.xml, ide.general.xml, other.xml, alibaba-toolkit-invitation.xml, _@user_Default.icls, lombok-plugin.xml, alibaba-toolkit-region.xml, other.xml, ui.lnf.xml, filetypes.xml, plugin_ui.xml, colors.scheme.xml, Xcode Midnight.xml, VisualVMLauncher.xml, alibaba-toolkit-settings.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, stringManipulation.xml, alibaba-toolkit-edas.xml, output.xml, OGNL _Struts 2_.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, vcs.xml, alibaba-toolkit-account.xml, toStringTemplates.xml, usageView.xml, sonarlint.xml

---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @description  $DESCRIPTION
+ * @author   刘雄康
+ * @date      ${YEAR}年${MONTH}月${DAY}日
+ * @version  v1.0
+ */
+public @interface ${NAME} {
+}
